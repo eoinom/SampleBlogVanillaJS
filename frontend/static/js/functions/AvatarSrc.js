34f2c8c255f93d5dab512d512422aa4b7966464f
Eoin <eoinom@gmail.com>
2022-03-03 22:27:08 +0000

    Update api address for avatar images

--- frontend/static/js/functions/AvatarSrc.js
+++ frontend/static/js/functions/AvatarSrc.js
@@ -1,5 +1,5 @@
 export default function AvatarSrc(username, size) {
   const userSlug = username.replace(/\s/g, '-');
-  const avatarSize = typeof(size) == 'number' ? size : 30;
-  return `https://api.adorable.io/avatars/${avatarSize}/${userSlug}.png`;
+  const avatarSize = typeof size == 'number' ? size : 30;
+  return `https://adorable-avatars.broken.services/${avatarSize}/${userSlug}.png`;
 }
